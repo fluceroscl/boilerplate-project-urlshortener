a80649f4ef2874faffa0ed7b6411035a15b9a69f
Fernando Lucero <fernando.lucero@gmail.com>
2024-10-08 01:05:25 +0000

    fin 2

--- index.js
+++ index.js
@@ -22,38 +22,38 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+const urlStore = {}; // Objeto para almacenar las URLs acortadas
+
 // Endpoint para acortar URL
 app.post('/api/shorturl', function(req, res) {
   const url = req.body.url;
   console.log(url);
-  
-  // Validar la URL
   try {
     const urlObj = new URL(url);
-    
-    // Verificar la URL con dns.lookup
     dns.lookup(urlObj.hostname, (err) => {
       if (err) {
         return res.json({ error: 'invalid url' });
       }
-      
-      // Aquí iría la lógica para acortar la URL
-      const shortUrlId = Math.floor(Math.random() * 10000); // Generar un ID corto aleatorio
-      res.json({ original_url: url, short_url: shortUrlId }); // Ejemplo de respuesta
+      const shortUrlId = Math.floor(Math.random() * 10000); 
+      urlStore[shortUrlId] = url; 
+      res.json({ original_url: url, short_url: shortUrlId }); 
     });
   } catch (error) {
     res.json({ error: 'invalid url' });
   }
 });
 
-
 // Ruta para redirigir a la URL original
 app.get('/api/shorturl/:url(*)', (req, res) => { // Permite URLs completas como parámetro
   const url = decodeURIComponent(req.params.url); // Decodificar la URL
   console.log('SHORT URL: ', url);
+
+  if (urlStore[url]) {
+    return res.redirect(urlStore[url]);
+  }
+
   try {
     const urlObj = new URL(url);
-
     dns.lookup(urlObj.hostname, (err) => {
       if (err) return res.redirect('/');
       return res.redirect(url); 
@@ -62,7 +62,6 @@ app.get('/api/shorturl/:url(*)', (req, res) => { // Permite URLs completas como 
       return res.redirect('/');
   }
 });
-
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
